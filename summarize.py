3243536c1e186897c5d393772eb3ec2c7326e67f
Adrien Pestel <pestouille@gmail.com>
2024-06-18 19:36:25 +0200

    fix typo

--- summarize.py
+++ summarize.py
@@ -6,7 +6,7 @@ from split_audio import split_audio
 from transcribe_audio import transcribe_audio
 from summarize_transcripts import summarize_transcripts
 
-def summaryze_audio(media_filename, llm):
+def summarize_audio(media_filename, llm):
     # Create parts directory
     os.makedirs('parts', exist_ok=True)
     split_audio(media_filename)
@@ -16,11 +16,11 @@ def summaryze_audio(media_filename, llm):
     clean_workdir()
 
 
-def summaryze_youtube(url, llm):
+def summarize_youtube(url, llm):
     media_file = download_audio(url)
     os.rename('tmp_audio.wav', media_file + '.wav')
     print('Downloaded audio file: {}'.format(media_file))
-    summaryze_audio(media_file + '.wav', llm)
+    summarize_audio(media_file + '.wav', llm)
 
 def clean_workdir():
     os.system('rm -rf parts')
@@ -35,10 +35,10 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--llm', choices=['ollama','gpt4o'], default='gpt4o')
     args = parser.parse_args()
     if args.youtube_url is not None:
-        summaryze_youtube(args.youtube_url, args.llm)
+        summarize_youtube(args.youtube_url, args.llm)
         exit(0)
     elif args.media_file is not None:
-        summaryze_audio(args.media_file, args.llm)
+        summarize_audio(args.media_file, args.llm)
         exit(0)
     parser.print_help()
 
