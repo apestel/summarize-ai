aaed80d9eb6579d6cd46a0cb8b62ea267a2e2073
Adrien Pestel <pestouille@gmail.com>
2024-06-24 23:27:58 +0200

    moving progressively to hugging face transformer lib

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,7 +1,13 @@
 import os
 import subprocess
+import torch
 
 def transcribe_audio(input_pattern):
     for audio in os.listdir('parts'):
         if audio.startswith('output') and audio.endswith('.wav'):
             subprocess.run(['insanely-fast-whisper', '--file-name', f'parts/{audio}', '--transcript-path', f'parts/{audio}.json'])
+
+
+if __name__ == '__main__':
+    print("MPS available: ", torch.backends.mps.is_available())
+    print("CUDA available: ", torch.cuda.is_available())
